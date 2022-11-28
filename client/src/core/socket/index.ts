a09d80bf0a379ae9d089da0af352a00a31ae4a89
alwafor <alwafor1@gmail.com>
2022-11-28 09:34:08 +0300

    create socket on client sde

---
+++ client/src/core/socket/index.ts
@@ -0,0 +1,12 @@
+import { io, ManagerOptions, SocketOptions } from 'socket.io-client';
+
+const options: Partial<ManagerOptions & SocketOptions> = {
+  forceNew: true,
+  reconnectionAttempts: Infinity,
+  timeout: 10000,
+  transports: ['websocket'],
+};
+
+const socket = io('http://localhost:3000', options);
+
+export default socket;
